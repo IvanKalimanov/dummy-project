ee93f1cc5399f865cdcac21a4ecf2ca35243e3a6
IvanKalimanov <ikalimanov@mail.ru>
2021-12-12 12:16:21 +0300

    fix

--- dummy-server/main.go
+++ dummy-server/main.go
@@ -1,19 +1,19 @@
 package main
- 
+
 import (
 	"log"
 	"net/http"
 )
- 
+
 func main() {
 	http.HandleFunc("/hello/sayHello", echoPayload)
-	log.Printf("Go Backend: { HTTPVersion = 1 }; serving on http://localhost:9191/hello/sayHello")
+	log.Printf("dummy-server serving on http://localhost:5000/hello/sayHello")
 	log.Fatal(http.ListenAndServe(":5000", nil))
 }
- 
+
 func echoPayload(w http.ResponseWriter, req *http.Request) {
 	log.Printf("Request connection: %s, path: %s", req.Proto, req.URL.Path[1:])
 	defer req.Body.Close()
 	w.WriteHeader(http.StatusOK)
-    w.Write([]byte("Hello, world!"))
+	w.Write([]byte("Hello, world!"))
 }
